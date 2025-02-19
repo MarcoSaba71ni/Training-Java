997caeff6862b90734b2c3aa3d18c45e383467b8
Marco Sabatini <marsab04188@stud.noroff.no>
2025-02-19 10:40:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+ async function fetchProducts() {
+    try {
+        const response = await fetch("https://v2.api.noroff.dev/rainy-days");
+        if (!response.ok) {
+          throw new Error("Can't fetch")};
  
+        const data = await response.json();
+       console.log(data);
+    } catch (error) {
+        console.error("Error fetching products:", error);
+    }
+} 
+
+// Replacing rendered HTML Content for Js
+
+const displayContainer = document.getElementById('display-container');
+console.log(displayContainer); // The result is null because we didn't fetch the elements yet.
+
+function main () {
+    console.log('mains is working');
+}
+//
+
+
+
+
+console.log("HI");
+
+function main () {
+    console.log('mains is working');
+}
+
